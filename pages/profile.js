dafa376645e77714e6e3fe9f2c17de474910262b
MaggieChafee <maggie.chafee@gmail.com>
2024-04-27 11:13:54 -0500

    basic user profile completed

--- pages/profile.js
+++ pages/profile.js
@@ -1,9 +1,28 @@
-import React from 'react';
+/* eslint-disable @next/next/no-img-element */
+import React, { useEffect, useState } from 'react';
+import { getSingleUser } from '../api/userData';
+import { useAuth } from '../utils/context/authContext';
+// import { getReviewsByUser } from '../api/reviewData';
+// import { Button } from 'react-bootstrap';
 
 export default function Profile() {
+  const [singleUser, setSingleUser] = useState({});
+  const { user } = useAuth();
+
+  const getDetails = () => {
+    getSingleUser(user.id).then(setSingleUser);
+  };
+
+  useEffect(() => {
+    getDetails();
+  });
+
   return (
     <div>
-      User Profile Information
+      <img src={singleUser.image} alt="profile" style={{ borderRadius: '50%', width: 250, height: 250 }} />
+      <h1>Hello {singleUser.name}! </h1>
+      <p>Email: {singleUser.email}</p>
+      <p>Click the button below to logout!</p>
     </div>
   );
 }
