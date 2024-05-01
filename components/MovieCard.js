98bd149c01703f5df50debc26a77ff47341d23b0
MaggieChafee <maggie.chafee@gmail.com>
2024-04-30 22:26:54 -0500

    more styling completed

--- components/MovieCard.js
+++ components/MovieCard.js
@@ -1,24 +1,29 @@
+/* eslint-disable import/no-extraneous-dependencies */
 import React from 'react';
 import Card from 'react-bootstrap/Card';
 import Button from 'react-bootstrap/Button';
 import { PropTypes } from 'prop-types';
 import Link from 'next/link';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faEye } from '@fortawesome/free-solid-svg-icons';
 
 export default function MovieCard({ movieObj }) {
   return (
-    <Card className="card-style">
+    <Card className="card-style" style={{ height: '550px' }}>
       <Card.Img variant="top" src={movieObj.image} alt={movieObj.title} style={{ height: '350px' }} />
       <Card.Body>
-        <title>{movieObj.title}</title>
+        <h4>{movieObj.title.toUpperCase()}</h4>
         <div className="card-genres">
           {movieObj.genres?.map((genre) => (
             <p key={genre.id} className="card-individual-genre">{genre.name}</p>
           ))}
         </div>
-        <Link href={`/movie/${movieObj.id}`} passHref>
-          <Button variant="primary">VIEW</Button>
-        </Link>
-        <Button variant="info">WISHLIST</Button>
+        <div>
+          <Link href={`/movie/${movieObj.id}`} passHref>
+            <Button variant="dark" style={{ width: '25%' }}><FontAwesomeIcon icon={faEye} style={{ color: '#683ce4' }} /></Button>
+          </Link>
+          <Button className="card-btn" style={{ width: '75%' }} variant="dark">+ WATCHLIST</Button>
+        </div>
       </Card.Body>
     </Card>
   );
