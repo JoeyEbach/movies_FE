cf70f91ee70e1db99a8ae1ac7a60686f8ea80940
MaggieChafee <maggie.chafee@gmail.com>
2024-05-06 20:09:46 -0500

    functionality check

--- components/MovieCard.js
+++ components/MovieCard.js
@@ -4,9 +4,20 @@ import Card from 'react-bootstrap/Card';
 import Button from 'react-bootstrap/Button';
 import { PropTypes } from 'prop-types';
 import Link from 'next/link';
+import { useAuth } from '../utils/context/authContext';
+import { addToWatchlist } from '../api/movieData';
 
 export default function MovieCard({ movieObj }) {
   // const [watchlistMovies, setWatchlistMovies] = useState({});
+
+  const { user } = useAuth();
+
+  const addToMyWatchlist = () => {
+    if (window.confirm(`add ${movieObj.title} to watchlist?`)) {
+      const payload = { userId: user.id, movieId: movieObj.id };
+      addToWatchlist(payload);
+    }
+  };
 
   return (
     <Card className="card-style" style={{ height: '550px' }}>
@@ -15,7 +26,7 @@ export default function MovieCard({ movieObj }) {
       </Link>
       <Card.Body>
         <div>
-          <Button className="card-btn" style={{ width: '75%' }} variant="dark">+ WATCHLIST</Button>
+          <Button className="card-btn" style={{ width: '75%' }} variant="dark" onClick={addToMyWatchlist}>+ WATCHLIST</Button>
         </div>
         <h4>{movieObj.title.toUpperCase()}</h4>
         <div className="card-genres">
